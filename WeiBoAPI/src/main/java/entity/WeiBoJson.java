56ec5224e83653fe1bab4b24aa2642b3f1b76d58
dsttl3 <dsttl3@sina.cn>
2019-03-09 22:04:14 +0800

    Create WeiBoJson.java

---
+++ WeiBoAPI/src/main/java/entity/WeiBoJson.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class WeiBoJson {
+	String access_token;
+	String remind_in;
+	String expires_in;
+	String uid;
+	String isRealName;
+	public String getAccess_token() {
+		return access_token;
+	}
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	public String getRemind_in() {
+		return remind_in;
+	}
+	public void setRemind_in(String remind_in) {
+		this.remind_in = remind_in;
+	}
+	public String getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(String expires_in) {
+		this.expires_in = expires_in;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getIsRealName() {
+		return isRealName;
+	}
+	public void setIsRealName(String isRealName) {
+		this.isRealName = isRealName;
+	}
+}
