a7e2f6b277931de4687a2788eda571c79871b8a4
dsttl3 <dsttl3@sina.cn>
2019-03-10 11:00:58 +0800

    Create Test.java

---
+++ WeiBoAPI/src/main/java/index/Test.java
@@ -0,0 +1,14 @@
+package index;
+
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) {
+		try {
+			System.out.println(new ShareWeiBo().share());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
