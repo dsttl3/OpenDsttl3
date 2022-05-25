76957a4dcac022c61f8c08148b0b8bbb4b666d75
dsttl3 <admin@dsttl3.cn>
2022-05-26 00:47:23 +0800

    -

--- WeiBoAPI/src/main/java/index/Test.java
+++ WeiBoAPI/src/main/java/index/Test.java
@@ -2,13 +2,16 @@ package index;
 
 import java.io.IOException;
 
+/**
+ * @author dsttl3
+ */
 public class Test {
-	public static void main(String[] args) {
-		try {
-			System.out.println(new ShareWeiBo().share());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            System.out.println(new ShareWeiBo().share());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
